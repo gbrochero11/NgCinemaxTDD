85bd739df72dce1fd8f275035ebddb8ee5a7c540
Gianluca Brochero <gbrochero11@outlook.es>
2020-01-30 15:34:34 -0500

    Navbar Social

---
+++ src/app/landing-page/navbar-social/navbar-social.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-social',
+  templateUrl: './navbar-social.component.html',
+  styleUrls: ['./navbar-social.component.scss']
+})
+export class NavbarSocialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
