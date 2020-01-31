c9e6ec37784f86fb23905fba33ea7ffdb9efd983
Gianluca Brochero <gbrochero11@outlook.es>
2020-01-31 09:21:18 -0500

    premier section

---
+++ src/app/landing-page/premieres/premieres.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-premieres',
+  templateUrl: './premieres.component.html',
+  styleUrls: ['./premieres.component.scss']
+})
+export class PremieresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
